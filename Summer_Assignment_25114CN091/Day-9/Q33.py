dee272bf05f730574edc878f723a5336281a6644
GLB-25114CN091 <vedanshsaini12may@gmail.com>
2026-06-05 20:15:04 +0530

    Day-9 uploaded

---
+++ Summer_Assignment_25114CN091/Day-9/Q33.py
@@ -0,0 +1,4 @@
+rows = 5  
+# Loop through rows
+for i in range(rows, 0, -1):  
+    print("*" * i)  
---
+++ Summer_Assignment_25114CN091/Day-9/Q35.py
@@ -0,0 +1,4 @@
+
+for i in range(5):  
+    char = chr(65 + i)  #  (65 = 'A')
+    print(char * (i + 1))  
